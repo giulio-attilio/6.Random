99b0ba00ac73bbe5db7a95dc2d7e60c8462e5af0
giulio-attilio <giulioap@hotmail.com>
2023-06-19 12:41:32 -0300

    1st Commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+const drawButton = document.getElementsByClassName("draw-button")
+const inputFrom = document.getElementsByClassName("input-from")
+const inputTo = document.getElementsByClassName("input-to")
+
+drawButton.addEventListener("click", generateRandom)
+
