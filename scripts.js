80d0bb6c97ee73e39d156ed4cf85b1164bb851d1
giulio-attilio <giulioap@hotmail.com>
2023-06-22 11:14:09 -0300

    Add if in script.js and some adjusts in styles.css

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
 
 const drawButton = document.querySelector(".draw-button")
 
-
 drawButton.addEventListener("click", generagetRandom)
 
 function generagetRandom(min, max) {
     const inputFrom = document.querySelector(".input-from").value
     const inputTo = document.querySelector(".input-to").value
-
     min = Math.ceil(inputFrom)
     max = Math.floor(inputTo)
-    alert(Math.floor(Math.random() * (max - min + 1)) + min)
+
+    if (min < max) {
+
+        const result = (Math.floor(Math.random() * (max - min + 1)) + min)
+
+        alert(result)
+
+    } else {
+        alert('"From" MUST be lower than "To"')
+    }
+
 }
 
